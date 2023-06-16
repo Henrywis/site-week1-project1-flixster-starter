526c61ca856754d726b3f14f3a549004845a4338
Henry Alumona <henryalumona@meta.com>
2023-06-16 03:04:28 -0700

    Modified logo in HTML,CSS & implemented search

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function createMovieCard(movie) {
 
     const votes = document.createElement("p");
     votes.classList.add("movie-votes");
-    votes.textContent = `Votes: ${movie.vote_count}`;
+    votes.textContent = `⭐ Votes: ${movie.vote_average}`;
     movieCard.appendChild(votes);
 
     return movieCard;
@@ -38,40 +38,22 @@ submitBtn.addEventListener("click", async (event) => {
     try {
         const apiKey = "26a8b5e8111d2b3fd500afd21defd060";
         const searchTerm = searchInput.value;
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&q=${encodeURIComponent(searchTerm)}`; //searchTerm is what is taken in from the user's input
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${(searchTerm)}`; //searchTerm is what is taken in from the user's input
 
-        const response = await fetch(url); // response is a string. "{movies: ["tile": "John Wick"]}"
+        const response = await fetch(url);                   // response is a string. "{movies: ["tile": "John Wick"]}"
         const container = await response.json();
         const movie_temp = container.results;
 
-        movieDiv.innerHTML = ''; //clears prev movie cards
+        moviesGrid.innerHTML = ''; //clears prev movie cards
 
         movie_temp.forEach(movie => {
-            const movieCard = createMovieCard(movie); //creates a Div element in (thru js) for each movie card
-            // movieCard.classList.add("movie-card"); //adds a class to be accessed and styled
-
-            // const img = document.createElement("img"); //creates img element dynamically in js
-            // img.classList.add("movie-poster"); // makes the class for img in CSS to be accessed as movie-poster
-
-            // const path = "http://image.tmdb.org/t/p/w154"
-            // img.src = path + movie.poster_path;
-            // movieCard.appendChild(img); // Step 3: Add the image url to the moviesDiv
-
-            // const title = document.createElement("h3");
-            // title.classList.add("movie-title");
-            // title.textContent = movie.title;
-            // movieCard.appendChild(title);
-
-            // const votes = document.createElement("p");
-            // votes.classList.add("movie-votes");
-            // votes.textContent = `Votes: ${movie.vote_count}`;
-            // movieCard.appendChild(votes);
-
-            movieDiv.appendChild(movieCard);
+            const movieCard = createMovieCard(movie);        //creates a Div element in (thru js) for each movie card
+            moviesGrid.appendChild(movieCard);
         });
 
         searchInput.value = '';
         console.log(container);
+
         // Process the data returned from the API
     } catch (error) {
         // Handle any errors that occur during the fetch request
@@ -121,34 +103,12 @@ async function displayAllPictures() {
         const container = await response.json();
         const movie_temp = container.results;
 
-        
-        //   const moviesGrid = document.createElement("div"); //making the default page have the movies grid in a div
-        //   moviesGrid.id = "movies-grid";
-
         movie_temp.forEach(movie => {
-            const movieCard = createMovieCard(movie); //Each movie will have its movie card in a div
-            // movieCard.classList.add("movie-card");           //creating a class for the cards to have a generic edit
-
-            // const img = document.createElement("img");       
-            // img.classList.add("movie-poster");               //img container storing the poster and having a class to edit generically
-            // const path = "http://image.tmdb.org/t/p/w154";
-            // img.src = path + movie.poster_path;
-            // movieCard.appendChild(img);                     // Step 3: Add the image url to the moviesCard div
-
-            // const title = document.createElement("h3");     //new h3 element for title
-            // title.classList.add("movie-title");             //edit all titles in same class
-            // title.textContent = movie.title;                //source the titles from the results
-            // movieCard.appendChild(title);                   //add each title to each card
-
-            // const votes = document.createElement("p");      //do same for votes
-            // votes.classList.add("movie-votes");
-            // votes.textContent = `Votes: ${movie.vote_count}`;
-            // movieCard.appendChild(votes);
-
-            moviesGrid.appendChild(movieCard);              //append each card(with complete detail) to the Grids
+            const movieCard = createMovieCard(movie);            //Each movie will have its movie card in a div
+            moviesGrid.appendChild(movieCard);                   //append each card(with complete detail) to the Grids
         });
 
-        //   movieDiv.appendChild(moviesGrid);                 //fit all complete grids into the div
+        //   movieDiv.appendChild(moviesGrid);                   //fit all complete grids into the div
         console.log(container);
         currentPage++
     } catch (error) {
@@ -165,5 +125,4 @@ displayAllPictures();
 loadMoreBtn.addEventListener("click", (event) => {
     event.preventDefault();
     displayAllPictures();
-    // currentPage++;
 });
