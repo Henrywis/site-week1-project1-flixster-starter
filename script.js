b7867531412a4d7ec8f594eaab794be36497acb6
Henry Alumona <henryalumona@meta.com>
2023-06-16 02:27:10 -0700

    Modified the Js to load more and close btn

--- script.js
+++ script.js
@@ -3,8 +3,33 @@ var searchInput = document.getElementById("searchInput");
 var submitBtn = document.getElementById("submitBtn");
 var closeSearchBtn = document.getElementById("closeSearchBtn");
 var moviesGrid = document.getElementById("movies-grid");
+var loadMoreBtn = document.getElementById("load-more-movies-btn");
 var movieDiv = document.getElementById("movieDiv");
-var currentPage = 1;
+let currentPage = 1;
+
+//Instead of repeating process, create function for movie cards
+function createMovieCard(movie) {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    const img = document.createElement("img");
+    img.classList.add("movie-poster");
+    const path = "http://image.tmdb.org/t/p/w154";
+    img.src = path + movie.poster_path;
+    movieCard.appendChild(img);
+
+    const title = document.createElement("h3");
+    title.classList.add("movie-title");
+    title.textContent = movie.title;
+    movieCard.appendChild(title);
+
+    const votes = document.createElement("p");
+    votes.classList.add("movie-votes");
+    votes.textContent = `Votes: ${movie.vote_count}`;
+    movieCard.appendChild(votes);
+
+    return movieCard;
+}
 
 //function to display result based on search
 submitBtn.addEventListener("click", async (event) => {
@@ -22,26 +47,26 @@ submitBtn.addEventListener("click", async (event) => {
         movieDiv.innerHTML = ''; //clears prev movie cards
 
         movie_temp.forEach(movie => {
-            const movieCard = document.createElement("div"); //creates a Div element in (thru js) for each movie card
-            movieCard.classList.add("movie-card"); //adds a class to be accessed and styled
-      
-            const img = document.createElement("img"); //creates img element dynamically in js
-            img.classList.add("movie-poster"); // makes the class for img in CSS to be accessed as movie-poster
+            const movieCard = createMovieCard(movie); //creates a Div element in (thru js) for each movie card
+            // movieCard.classList.add("movie-card"); //adds a class to be accessed and styled
 
-            const path = "http://image.tmdb.org/t/p/w154"
-            img.src = path + movie.poster_path;
-            movieCard.appendChild(img); // Step 3: Add the image url to the moviesDiv
+            // const img = document.createElement("img"); //creates img element dynamically in js
+            // img.classList.add("movie-poster"); // makes the class for img in CSS to be accessed as movie-poster
 
-            const title = document.createElement("h3");
-            title.classList.add("movie-title");
-            title.textContent = movie.title;
-            movieCard.appendChild(title);
-      
-            const votes = document.createElement("p");
-            votes.classList.add("movie-votes");
-            votes.textContent = `Votes: ${movie.vote_count}`;
-            movieCard.appendChild(votes);
-      
+            // const path = "http://image.tmdb.org/t/p/w154"
+            // img.src = path + movie.poster_path;
+            // movieCard.appendChild(img); // Step 3: Add the image url to the moviesDiv
+
+            // const title = document.createElement("h3");
+            // title.classList.add("movie-title");
+            // title.textContent = movie.title;
+            // movieCard.appendChild(title);
+
+            // const votes = document.createElement("p");
+            // votes.classList.add("movie-votes");
+            // votes.textContent = `Votes: ${movie.vote_count}`;
+            // movieCard.appendChild(votes);
+
             movieDiv.appendChild(movieCard);
         });
 
@@ -57,75 +82,88 @@ submitBtn.addEventListener("click", async (event) => {
     console.log("Button clicked!");
 })
 
-// Function to display movies for a specific page
-// async function displayMovies(page) {
-//     try {
-//       const apiKey = "26a8b5e8111d2b3fd500afd21defd060";
-//       const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&page=${page}`;
-  
-//       const response = await fetch(url);
-//       const data = await response.json();
-//       const movies = data.results;
-  
-//       movies.forEach(movie => {
-//         const movieCard = createMovieCard(movie);
-//         moviesGrid.appendChild(movieCard);
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
+//Function to display movies for a specific page
+async function displayMovies(page) {
+    try {
+        const apiKey = "26a8b5e8111d2b3fd500afd21defd060";
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${page}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        const movies = data.results;
+
+        movies.forEach(movie => {
+            const movieCard = createMovieCard(movie);
+            moviesGrid.appendChild(movieCard);
+        });
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
 closeSearchBtn.addEventListener("click", () => {
+    currentPage = 1
+    moviesGrid.innerHTML = "";
     displayAllPictures();
-  });
+});
 
 
 // Function to display all pictures by default
 async function displayAllPictures() {
     try {
-      const apiKey = "26a8b5e8111d2b3fd500afd21defd060";
-      const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
-  
-      const response = await fetch(url);
-      const container = await response.json();
-      const movie_temp = container.results;
+        const apiKey = "26a8b5e8111d2b3fd500afd21defd060";
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${currentPage}`;
 
-      movieDiv.innerHTML = '';
+        const response = await fetch(url);
+        const container = await response.json();
+        const movie_temp = container.results;
 
-      const moviesGrid = document.createElement("div"); //making the default page have the movies grid in a div
-      moviesGrid.id = "movies-grid";
+        
+        //   const moviesGrid = document.createElement("div"); //making the default page have the movies grid in a div
+        //   moviesGrid.id = "movies-grid";
 
-      movie_temp.forEach(movie => {
-        const movieCard = document.createElement("div"); //Each movie will have its movie card in a div
-        movieCard.classList.add("movie-card");           //creating a class for the cards to have a generic edit
+        movie_temp.forEach(movie => {
+            const movieCard = createMovieCard(movie); //Each movie will have its movie card in a div
+            // movieCard.classList.add("movie-card");           //creating a class for the cards to have a generic edit
 
-        const img = document.createElement("img");       
-        img.classList.add("movie-poster");               //img container storing the poster and having a class to edit generically
-        const path = "http://image.tmdb.org/t/p/w154";
-        img.src = path + movie.poster_path;
-        movieCard.appendChild(img);                     // Step 3: Add the image url to the moviesCard div
+            // const img = document.createElement("img");       
+            // img.classList.add("movie-poster");               //img container storing the poster and having a class to edit generically
+            // const path = "http://image.tmdb.org/t/p/w154";
+            // img.src = path + movie.poster_path;
+            // movieCard.appendChild(img);                     // Step 3: Add the image url to the moviesCard div
 
-        const title = document.createElement("h3");     //new h3 element for title
-        title.classList.add("movie-title");             //edit all titles in same class
-        title.textContent = movie.title;                //source the titles from the results
-        movieCard.appendChild(title);                   //add each title to each card
-  
-        const votes = document.createElement("p");      //do same for votes
-        votes.classList.add("movie-votes");
-        votes.textContent = `Votes: ${movie.vote_count}`;
-        movieCard.appendChild(votes);
+            // const title = document.createElement("h3");     //new h3 element for title
+            // title.classList.add("movie-title");             //edit all titles in same class
+            // title.textContent = movie.title;                //source the titles from the results
+            // movieCard.appendChild(title);                   //add each title to each card
 
-        moviesGrid.appendChild(movieCard);              //append each card(with complete detail) to the Grids
-      });
-  
-      movieDiv.appendChild(moviesGrid);                 //fit all complete grids into the div
-      console.log(container);
+            // const votes = document.createElement("p");      //do same for votes
+            // votes.classList.add("movie-votes");
+            // votes.textContent = `Votes: ${movie.vote_count}`;
+            // movieCard.appendChild(votes);
+
+            moviesGrid.appendChild(movieCard);              //append each card(with complete detail) to the Grids
+        });
+
+        //   movieDiv.appendChild(moviesGrid);                 //fit all complete grids into the div
+        console.log(container);
+        currentPage++
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
-  
-  // Call the displayAllPictures function to show all pictures by default
-  displayAllPictures();
+}
+
+// Call the displayAllPictures function to show all pictures by default
+displayAllPictures();
+
+
+// Event listener for the "Load More" button
+
+loadMoreBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    displayAllPictures();
+    // currentPage++;
+});
